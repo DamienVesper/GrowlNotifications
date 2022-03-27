a29fdfe93e6f0ae68e43ccab262260086f5e1998
Damien Vesper <ldamienvesper@gmail.com>
2022-03-27 09:50:41 -0500

    update mergings

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -1,13 +1,47 @@
-import path from 'path';
 import * as Webpack from 'webpack';
+import * as path from 'path';
 
-import HTMLWebpackPlugin from 'html-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import WDS from 'webpack-dev-server';
 
-import { WebpackConfiguration } from 'webpack-dev-server';
+interface Configuration extends Webpack.Configuration {
+    devServer?: WDS.Configuration
+}
 
-const config: Webpack.Configuration = {
+const config: Configuration = {
+    entry: path.resolve(__dirname, `../src/index.ts`),
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: [
+                    {
+                        loader: `ts-loader`,
+                        options: {
+                            transpileOnly: true,
+                            experimentalWatchApi: true
+                        }
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: `asset/resource`
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: `asset/resource`
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [`*`, `.js`, `.ts`]
+    },
+
+    plugins: [
+        new Webpack.ProgressPlugin()
+    ]
 };
 
 export default config;
---
+++ webpack/webpack.dev.ts
@@ -0,0 +1,55 @@
+import merge from 'webpack-merge';
+import common from './webpack.common';
+
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+
+import * as path from 'path';
+
+const config = merge(common, {
+    mode: `development`,
+    devtool: `source-map`,
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    `style-loader`,
+                    `css-loader`,
+                    `sass-loader`
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [`style-loader`, `css-loader`]
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: true,
+            template: path.resolve(__dirname, `../public/index.html`),
+            favicon: `./public/assets/img/logos/favicon.png`
+        })
+    ],
+
+    output: {
+        path: path.resolve(__dirname, `../dist`),
+        filename: `bundle.min.js`
+    },
+
+    devServer: {
+        devMiddleware: {
+            publicPath: `http://localhost:3000`
+        },
+        static: {
+            directory: path.resolve(__dirname, `../public`)
+        },
+        historyApiFallback: true,
+        port: 3000,
+        hot: `only`
+    }
+});
+
+export default config;
